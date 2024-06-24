fcf0919d9c6de68029a52a758ffcf95bc72fb500
Vaibhav Salunke <vaibhav.salunke.ori@gmail.com>
2024-06-24 22:02:40 +0530

    config

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import { sveltekit, vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    })
   },
+  prerender:{default:true},
   preprocess: vitePreprocess()
 };
 export default config;
